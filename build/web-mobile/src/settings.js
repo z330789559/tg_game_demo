4388d5db6fbf600375fea42232ec5911b7788dfb
zhang <2672961671@qq.com>
2024-09-02 15:47:14 +0800

    打包测试

---
+++ build/web-mobile/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:["main"],launchScene:"db://assets/Game.fire",orientation:"portrait",jsList:[]};
