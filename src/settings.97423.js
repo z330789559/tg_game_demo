318218b3ac94a8f14863d4e354997921c7cd3fb9
libaozhong <baozhongmail@yeah.net>
2024-09-30 09:18:47 +0800

    change

---
+++ src/settings.97423.js
@@ -0,0 +1,32 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "ui"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Game.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [
+        "assets/scripts/plugins/telegramUi.7d591.js",
+        "assets/scripts/plugins/tonWeb.c5392.js"
+    ],
+    bundleVers: {
+        internal: "be8c4",
+        resources: "854f8",
+        main: "d3e22"
+    }
+};
